1254945935b09a59eae50646254c1fd71e53359e
NaMcO <namcojr@gmail.com>
2026-09-06 08:49:38 +0100

    removed temp path

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -23,7 +23,7 @@ android {
 
     signingConfigs {
         create("release") {
-            storeFile = file(System.getenv("KEYSTORE_PATH") ?: "/home/namco/.keystores/home-release.jks")
+            storeFile = file(System.getenv("KEYSTORE_PATH")
             storePassword = System.getenv("KEYSTORE_PASSWORD")
             keyAlias = System.getenv("KEY_ALIAS")
             keyPassword = System.getenv("KEY_PASSWORD")
